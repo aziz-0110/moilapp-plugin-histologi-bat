5daf435e6ca8360a149be136fe621c58dfbe79ce
aziz-0110 <222degree@gmail.com>
2024-04-11 08:16:16 +0700

    add function graph

--- contoller.py
+++ contoller.py
@@ -218,7 +218,33 @@ class Controller(QWidget):
         end_crop.pop(0)
         return start_crop, end_crop
 
+    def graph(self):    # untuk grafik
 
+        # Fixing random state for reproducibility
+        np.random.seed(19680801)
+
+        dt = 0.01
+        t = np.arange(0, 30, dt)
+        nse1 = np.random.randn(len(t))                 # white noise 1
+        nse2 = np.random.randn(len(t))                 # white noise 2
+
+        # Two signals with a coherent part at 10 Hz and a random part
+        s1 = np.sin(2 * np.pi * 10 * t) + nse1
+        s2 = np.sin(2 * np.pi * 10 * t) + nse2
+
+        fig, axs = plt.subplots(2, 1, layout='constrained')
+        axs[0].plot(t, s1, t, s2)
+        axs[0].set_xlim(0, 2)
+        axs[0].set_xlabel('Time (s)')
+        axs[0].set_ylabel('s1 and s2')
+        axs[0].grid(True)
+
+        cxy, f = axs[1].cohere(s1, s2, 256, 1. / dt)
+        axs[1].set_ylabel('Coherence')
+
+        plt.savefig(f"{self.path_img_save}/tmp/graph.png")
+
+        plt.show()
 
 class HistologiBat(PluginInterface):
     def __init__(self):
